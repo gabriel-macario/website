b6ab28728e052044b1231b4fe8074f2933b76883
Aaron <aaronssy1@gmail.com>
2018-10-24 13:19:40 -0700

    added landing page with particlesJS

---
+++ my-portfolio/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ("./Home.css")
+
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className = "homeContainer">
+       
+        <p className = "text-center">Aaron Sy </p>
+        
+      </div>
+    )
+  }
+}
---
+++ my-portfolio/src/components/Particle/Particle.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import Particles from 'react-particles-js';
+import ("./Particle.css");
+
+const particleOpt = {
+    particles: {
+     number:{
+       value: 15,
+       density:{
+         enable: true,
+         value_area: 800,
+      }
+     }
+    }
+  }
+
+export default class HomeCompliment extends Component {
+  render() {
+    return (
+      
+        <Particles
+        className = "background"
+          params={particleOpt}
+        />
+      
+    )
+  }
+}
